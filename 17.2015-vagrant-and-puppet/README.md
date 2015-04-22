a7d95137a43fabaa62d584ae1c78922edc244f77
Grisha Pushkov <grisha@push.org.ru>
2015-04-22 22:29:09 +0300

    base environment

--- 17.2015-vagrant-and-puppet/README.md
+++ 17.2015-vagrant-and-puppet/README.md
@@ -1,1 +1,5 @@
+# Vagrant and Puppet
 
+### Examples
+[patrickdlee/vagrant-examples](https://github.com/patrickdlee/vagrant-examples)
+[purple52/librarian-puppet-vagrant](https://github.com/purple52/librarian-puppet-vagrant)
--- 17.2015-vagrant-and-puppet/examples/simple/puppet/manifests/default.pp
+++ 17.2015-vagrant-and-puppet/examples/simple/puppet/manifests/default.pp
@@ -1,1 +1,13 @@
-class { 'nodejs':  }
+class { 'nodejs':
+  version      => 'stable',
+  make_install => false,
+}
+
+class { 'ruby':
+  version      => '2.1.5',
+}
+
+class { 'redis': }
+class { 'mongodb': }
+class { 'postgresql::server': }
+class { 'nginx': }
